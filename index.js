21e2be123039f10fddcead964d287944f069fb7e
Gaitano <gaitano.george@student.moringaschool.com>
2023-06-14 16:26:14 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun( activity = "roller-skate") {
+
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+
+function mondayWork ( activity ) {
+    if (!activity) {
+        return  `This Monday, I will go to the office.`
+    }
+    else {
+        return `This Monday, I will work from home.`
+    }
+}
+
+
+function wrapAdjective (flair = "*") {
+    return function (adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
